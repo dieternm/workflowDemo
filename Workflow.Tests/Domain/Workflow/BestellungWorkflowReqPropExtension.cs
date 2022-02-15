bc200d83189540ed5307d6bce71e550dddf15445
Dieter <Nunnenmacher>
2022-02-15 21:37:14 +0100

    Minor changes

--- Workflow.Tests/Domain/Workflow/BestellungWorkflowReqPropExtension.cs
+++ Workflow.Tests/Domain/Workflow/BestellungWorkflowReqPropExtension.cs
@@ -8,10 +8,9 @@ namespace Workflow.Tests.Domain.Workflow
         protected override IEnumerable<PropertyInfo> GetRequiredProperties(Bestellung participant)
         {
             switch (participant.State)
-
             {
                 case BestellungWorkflowState.InVersand:
-                    yield return PropertyHelper.GetPropertyInfo((Bestellung Bestellung) => Bestellung.Empfänger);
+                    yield return PropertyHelper.GetPropertyInfo((Bestellung b) => b.Empfänger);
                     break;
                 case BestellungWorkflowState.Storniert:
                 default:
