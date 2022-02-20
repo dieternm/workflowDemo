350eaae6fed6281d1e7243aeefcf55c17f48d8db
Dieter <Nunnenmacher>
2022-02-20 19:03:02 +0100

    move GetRequiredProperties to BestellungWorkflow.cs

--- Workflow.Tests/Domain/Workflow/BestellungWorkflow.cs
+++ Workflow.Tests/Domain/Workflow/BestellungWorkflow.cs
@@ -1,4 +1,7 @@
-﻿namespace Workflow.Tests.Domain.Workflow
+﻿using System.Reflection;
+using Workflow.Tests.Utils;
+
+namespace Workflow.Tests.Domain.Workflow
 {
     public partial class BestellungWorkflow : Workflow<Bestellung, BestellungWorkflowState, BestellungWorkflowOperation>
     {
@@ -34,6 +37,19 @@
                 case BestellungWorkflowState.Geprueft:
                     yield return BestellungWorkflowOperation.versenden;
                     break;
+                default:
+                    yield break;
+            }
+        }
+
+        protected override IEnumerable<PropertyInfo> GetRequiredProperties(Bestellung participant, BestellungWorkflowState state)
+        {
+            switch (state)
+            {
+                case BestellungWorkflowState.InVersand:
+                    yield return PropertyHelper.GetPropertyInfo((Bestellung b) => b.Empfaenger);
+                    break;
+                case BestellungWorkflowState.Storniert:
                 default:
                     yield break;
             }
